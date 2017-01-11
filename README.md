7c9e0b26c6e7c95543be5ca3960c4420da310f14
Caitlin Stein <caitlin.teter@gmail.com>
2017-01-11 09:59:45 -0600

    created a super-basic angular example without routing

---
+++ README.md
@@ -0,0 +1,8 @@
+#angularFun is a teaching tool intended to introduce angular
+
+Branches are used in the following order
+- master
+- basic: basic angular app w/ routing
+- http: $http "get", ng-repeat, ng-if
+- extras: "otherwise" added to routing, and an example of $routeParams
+- modularity: broke controllers, directives, and factories into their own files and included them in index.html
--- app/main.js
+++ app/main.js
@@ -1,38 +1,11 @@
 console.log("main.js here")
 //define your app
 
-var app = angular.module('nameOfApp', ['ngRoute'])
+var app = angular.module('nameOfApp', [])
 
-//configure your app (once)
-app.config(($routeProvider) => {
-    $routeProvider
-      .when('/', {
-        controller: 'MainCtrl',
-        templateUrl: 'partials/main.html',
-      })
-      .when('/list', {
-        controller: 'ListCtrl',
-        templateUrl: 'partials/list.html',
-      })
-  })
 //create controller(s)- as many as your want!
   app.controller('MainCtrl', function ($scope, $http) {
     console.log("main controller!")
     $scope.myVariable = "my variable"
 
   })
-  app.controller('ListCtrl', function ($scope, $http) {
-
-    console.log("list controller!")
-
-  })
-
-//create directives - as many as you want!
-app.directive("myDirective", function() {
-  //I guess some code goes here
-});
-
-//create factories - as many as you want!
-app.factory('myFactory', function() {  
-  //I guess some code goes here
-});
