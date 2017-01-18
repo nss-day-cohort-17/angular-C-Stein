d16a8136ea2383287140e7322a60ea4c15030561
Caitlin Stein <caitlin.teter@gmail.com>
2017-01-18 14:34:15 -0600

    improved structure of myFactory

--- app/factories/myFactory.js
+++ app/factories/myFactory.js
@@ -1,15 +1,14 @@
 app.factory('myFactory', function($http) { 
   console.log("myFactory"); 
 
-  function getListItems() {
-    return new Promise(function(resolve, reject) {
-      $http.get(`list.json`)
-      .then(function(val){
-        console.log("list.json", val.data)
-        resolve(val.data)
-      })
+  return {
+    getListItems : () => {
+        return $http.get(`list.json`)
+        .then(function(val){
+          console.log("list.json", val.data)
+          return val.data
     })
+    }
   }
 
-  return getListItems();
 });
